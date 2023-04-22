6208bd389ce6907af64d4f398b4d642490bd1fd3
Gabor Sinka <hessor@gmail.com>
2023-04-22 16:18:42 +0200

    make letters equal

--- src/components/game/CurrentWord.tsx
+++ src/components/game/CurrentWord.tsx
@@ -10,12 +10,10 @@ const CurrentWord: React.FC<CurrentWordProps> = ({ currentWord }) => {
       {Array.from(currentWord).map((char, index) => (
         <div
           key={`current-word-char-${index}`}
-          className="px-2 border-b-2 border-neutral-500 "
+          className="h-8 w-8 px-1 border-b-2 border-neutral-500 text-center"
         >
           <span
-            className={`text-xl uppercase h-8 w-8 ${
-              char === "_" ? "opacity-0" : ""
-            }`}
+            className={`text-xl uppercase ${char === "_" ? "opacity-0" : ""}`}
           >
             {char}
           </span>
